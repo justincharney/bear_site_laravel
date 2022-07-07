a875c88d4cec183237ab550b803e3f9aa1e59354
Justin Charney <justin.charney@gmail.com>
2022-07-07 11:20:17 -0700

    Changed website title in layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laragigs</title>
+    <title>Bear's base</title>
 </head>
 
 <body>
@@ -34,7 +34,7 @@
                 },
             };
         </script>
-        <title>LaraGigs | Find Laravel Jobs & Projects</title>
+        <title>Bear's Base</title>
     </head>
 
     <body class="mb-48">
